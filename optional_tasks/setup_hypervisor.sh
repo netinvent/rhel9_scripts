1dc5b748a51546a5198ef450a44d34277631922f
deajan <ozy@netpower.fr>
2024-10-10 09:43:58 +0200

    hypervisor: Add override file, also get cmd results

--- optional_tasks/setup_hypervisor.sh
+++ optional_tasks/setup_hypervisor.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 
-## Hypervisor Installer 2024012801 for RHEL9
+## Hypervisor Installer 2024101001 for RHEL9
 
 # Requirements:
 # RHEL9 installed with NPF VMv4 profile incl. node exporter
 # XFS System partition 50G for system
 
+# optional, setup_hypervisor.conf file with variable overrides
+[ -f ./setup_hypervisor.conf ] && source ./setup_hypervisor.conf
 
-CRT_SUBJECT="/C=FR/O=Oranization/CN=${COMMON_NAME}/OU=RD/L=City/ST=State/emailAddress=contact@example.tld"
+# COCKPIT ALLOWED USER
+[ -z "${ADMIN_USER}" ] && ADMIN_USER=myuser
+
+# HARDWARE ID for NetPerfect hardware
+# or UNKNOWN for other hardware
+[ -z "${HARDWARE_ID}" ] && HARDWARE_ID="UNKNOWN"
+
+
+# Autosigned certificate information
+[ -z "${COMMON_NAME}" ] && COMMON_NAME=hyper.local
+[ -z "${EMAIL}" ] && EMAIL=contact@local.tld
+[ -z "${CITY}" ] && CITY=DetroitRockCity
+[ -z "${STATE}" ] && STATE=Kiss
+
+CERT_DIR=/etc/pki/tls
+TARGET_DIR=/etc/ssl/certs
+CRT_SUBJECT="/C=FR/O=Oranization/CN=${COMMON_NAME}/OU=RD/L=${CITY}/ST=${STATE}/emailAddress=${EMAIL}"
 
 
 script_dir=$(pwd)
@@ -15,179 +33,27 @@ result=0
 
 echo "#### Installing prerequisites ####"
 
-dnf install -y epel-release
-dnf install -y policycoreutils-python-utils
-dnf install -y virt-what smartmontools tuned net-snmp tar bzip2 lm_sensors
-dnf install -y qemu-kvm libvirt virt-install bridge-utils libguestfs-tools guestfs-tools cockpit cockpit-machines
+dnf install -y epel-release || result=1
+dnf install -y policycoreutils-python-utils || result=1
+dnf install -y virt-what net-snmp tar bzip2 || result=1
+dnf install -y qemu-kvm libvirt virt-install bridge-utils libguestfs-tools guestfs-tools cockpit cockpit-machines cockpit-pcp || result=1
 
 # Optional virt-manager + X11 support (does not work in readonly mode)
-dnf install -y virt-manager xorg-x11-xauth
+dnf install -y virt-manager xorg-x11-xauth || result=1
 
 echo "#### System tuning ####"
 # Don't log martian packets, obviously we'll get plenty
 # These are RHEL specific with ANSSI BP028 high profile
 sysctl -w net.ipv4.conf.all.log_martians=0
-sed -i 's/net.ipv4.conf.all.log_martians = 1/net.ipv4.conf.all.log_martians = 0/g' /etc/sysctl.d/99-sysctl.conf
+# THis is a link
+#sed -i 's/net.ipv4.conf.all.log_martians = 1/net.ipv4.conf.all.log_martians = 0/g' /etc/sysctl.d/99-sysctl.conf
 sed -i 's/net.ipv4.conf.all.log_martians = 1/net.ipv4.conf.all.log_martians = 0/g' /etc/sysctl.conf
-
-echo "#### Setting up tuned profiles ####"
-
-mkdir /etc/tuned/{npf-eco,npf-perf}
-
-cat << 'EOF' > /etc/tuned/npf-eco/tuned.conf
-[main]
-summary=NetPerfect Powersaver
-include=powersave
-
-# SETTINGS_VER 2023110301
-
-[cpu]
-# Use governor conservative whenever we can, if not, use powersave
-governor=conserative
-# The way we scale (set via cpupower set --perf-bias 0-15, 15 being most power efficient)
-energy_perf_bias=15
-# This will set the minimal frequency available (used with intel_pstate, which replaces cpufreq values
-min_perf_pct=1
-max_perf_pct=75
-
-[sysctl]
-# Never put 0, because of potentiel OOMs
-vm.swappiness=1
-# Keep watchguard active so our machine does not lay there for months without operating
-# nmi_watchdog is enabled while we do not operate the tunnel so the machine does not stay dead
-kernel.nmi_watchdog = 1
-
-##### Prevent blocking system on high IO
-
-#Percentage of system memory which when dirty then system can start writing data to the disks.
-vm.dirty_background_ratio = 1
-
-#Percentage of system memory which when dirty, the process doing writes would block and write out dirty pages to the disks.
-vm.dirty_ratio = 2
-
-# delay for disk commit
-vm.dirty_writeback_centisecs = 100
-
-[script]
-# ON RHEL8, we need to keep profile dir
-# ON RHEL9, relative path is enough
-#script=\${i:PROFILE_DIR}/script.sh
-script=script.sh
-EOF
-
-cat << 'EOF' > /etc/tuned/npf-perf/tuned.conf
-[main]
-summary=NetPerfect Performance
-include=network-latency
-
-# SETTINGS_VER 2023110301
-
-[cpu]
-# Use governor ondemand whenever we can, if not, use performance which will disable all frequency changes
-governor=ondemand
-# The way we scale (set via cpupower set --perf-bias 0-15, 15 being most powersave)
-energy_perf_bias=performance
-# This will set the minimal frequency available (used with intel_pstate, which replaces cpufreq values
-min_perf_pct=40
-max_perf_pct=100
-
-[sysctl]
-# Never put 0, because of potentiel OOMs
-vm.swappiness=1
-# Keep watchguard active so our machine does not lay there for months without operating
-# let's keep the nmi_watchdog disabled while we operate the tunnel so we get no interruptions
-kernel.nmi_watchdog = 0
-
-##### Prevent blocking system on high IO
-
-#Percentage of system memory which when dirty then system can start writing data to the disks.
-vm.dirty_background_ratio = 1
-
-#Percentage of system memory which when dirty, the process doing writes would block and write out dirty pages to the disks.
-vm.dirty_ratio = 2
-
-# delay for disk commit
-vm.dirty_writeback_centisecs = 100
-
-[script]
-# ON RHEL8, we need to keep profile dir
-# ON RHEL9, relative path is enough
-#script=\${i:PROFILE_DIR}/script.sh
-script=script.sh
-EOF
-
-cat << 'EOF' > /etc/tuned/npf-eco/script.sh
-#!/usr/bin/env bash
-
-SCRIPT_VER=2024011401
-
-# Powersave will keep low frequency no matter what. If available, use conservative. If not use powersave
-if cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors | grep conservative > /dev/null; then
-	governor=conservative
-else
-	governor=powersave
-fi
-
-min_freq=$(cpupower frequency-info | grep limits | awk '{print $3}')
-min_freq_unit=$(cpupower frequency-info | grep limits | awk '{print $4}')
-max_freq=$(cpupower frequency-info | grep limits | awk '{print $6}')
-max_freq_unit=$(cpupower frequency-info | grep limits | awk '{print $7}')
-
-# Calc max freq in eco mode, don't use bc anymore since it's probably not installed
-#max_freq_eco=$(bc <<< "scale=2; $max_freq/1.5")
-max_freq_eco=$(echo "print(round(${max_freq}/1.5, 2))" | python3)
-
-# Set governor, min and max freq
-cpupower frequency-set -g $governor -d ${min_freq}${min_freq_unit} -u ${max_freq_eco}${max_freq_unit}
-
-# Set perf bias to max eco
-cpupower set --perf-bias 15
-
-# Enable all idle states
-cpupower idle-set -E
-EOF
-
-cat << 'EOF' > /etc/tuned/npf-perf/script.sh
-#!/usr/bin/env bash
-
-SCRIPT_VER=2023110301
-
-# Performance will keep CPU freq at max all the time. Prefer ondemand if available
-if cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors | grep ondemand > /dev/null; then
-	governor=ondemand
-else
-	governor=performance
-fi
-
-min_freq=$(cpupower frequency-info | grep limits | awk '{print $3}')
-min_freq_unit=$(cpupower frequency-info | grep limits | awk '{print $4}')
-max_freq=$(cpupower frequency-info | grep limits | awk '{print $6}')
-max_freq_unit=$(cpupower frequency-info | grep limits | awk '{print $7}')
-
-# Set governor, min and max freq
-cpupower frequency-set -g $governor -d ${min_freq}${min_freq_unit} -u ${max_freq}${max_freq_unit}
-
-# Set perf bias to max perf
-cpupower set --perf-bias 0
-
-# Optional disable idle states depending on latency (> 100ms)
-cpupower idle-set -D 100
-EOF
-
-chmod +x /etc/tuned/{npf-eco,npf-perf}/script.sh
-
-systemctl enable --now tuned
-tuned-adm profile npf-eco
 
 echo "#### Setting up system certificate ####"
 
-CERT_DIR=/etc/pki/tls
-COMMON_NAME=hyper.netinvent.local
-TARGET_DIR=/etc/ssl/certs
-
 [ ! -d "${TARGET_DIR}" ] && mkdir "${TARGET_DIR}"
 
-openssl req -nodes -new -x509 -days 7300 -newkey rsa:4096 -keyout ${CERT_DIR}/private/${COMMON_NAME// /_}.key -subj "${CRT_SUBJECT}" -out ${CERT_DIR}/certs/${COMMON_NAME// /_}.crt
+openssl req -nodes -new -x509 -days 7300 -newkey rsa:4096 -keyout ${CERT_DIR}/private/${COMMON_NAME// /_}.key -subj "${CRT_SUBJECT}" -out ${CERT_DIR}/certs/${COMMON_NAME// /_}.crt  || result=1
 cat ${CERT_DIR}/private/${COMMON_NAME// /_}.key ${CERT_DIR}/certs/${COMMON_NAME// /_}.crt > ${TARGET_DIR}/${COMMON_NAME// /_}.pem
 
 echo "#### Setup SNMP ####"
@@ -209,16 +75,14 @@ EOF
 sed -i '/^view    systemview    included   .1.3.6.1.2.1.25.1.1$/ r /tmp/snmpd_part.conf' /etc/snmp/snmpd.conf
 
 echo "#### Setting up cockpit & performance logging ####"
-dnf install -y cockpit cockpit-machines cockpit-pcp
-systemctl enable pmcd
-systemctl start pmcd
-systemctl enable pmlogger
-systemctl start pmlogger
-systemctl enable --now cockpit.socket
-# NPF-MOD-USER: On ztl, the user would be zerotouch !!!
-# Cockpit sudo must work for npfmonitor
-usermod -aG wheel npfmonitor
-echo 'Defaults:npfmonitor !requiretty' >> /etc/sudoers
+systemctl enable pmcd || result=1
+systemctl start pmcd || result=1
+systemctl enable pmlogger || result=1
+systemctl start pmlogger || result=1
+systemctl enable --now cockpit.socket || result=1
+# Cockpit sudo must work for admin user
+usermod -aG wheel ${ADMIN_USER} || result=1
+echo 'Defaults:'${ADMIN_USER}' !requiretty' >> /etc/sudoers
 
 echo "#### Setup first ethernet interface as bridged to new bridge kvmbr0 ####"
 # ip -br l == ip print brief list of network interfaces
@@ -229,11 +93,11 @@ if [ -z ${iface} ]; then
 fi
 
 # Disable spanning tree so we don't interrupt existing STP infrastructure
-nmcli c add type bridge ifname kvmbr0 con-name kvmbr0 autoconnect yes bridge.stp no
-nmcli c modify kvmbr0 ipv4.method dhcp
-nmcli c add type bridge-slave ifname ${iface} master kvmbr0 autoconnect yes
-nmcli c up kvmbr0
-nmcli c del ${iface}
+nmcli c add type bridge ifname kvmbr0 con-name kvmbr0 autoconnect yes bridge.stp no || result=1
+nmcli c modify kvmbr0 ipv4.method dhcp || result=1
+nmcli c add type bridge-slave ifname ${iface} master kvmbr0 autoconnect yes || result=1
+nmcli c up kvmbr0 || result=1
+nmcli c del ${iface} || result=1
 
 echo "#### Setting up virualization ####"
 cat << 'EOF' > /etc/sysconfig/libvirt-guests
@@ -243,27 +107,13 @@ PARALLEL_SHUTDOWN=2
 SHUTDOWN_TIMEOUT=360
 SYNC_TIME=1
 EOF
-systemctl enable --now libvirtd
-systemctl enable --now libvirt-guests
+systemctl enable --now libvirtd || result=1
+systemctl enable --now libvirt-guests || result=1
 
 echo "#### Setup PCI Passthrough ####"
-grubby --update-kernel=ALL --args="intel_iommu=on"
-grub2-mkconfig -o /boot/grub2/grub.cfg
+grubby --update-kernel=ALL --args="intel_iommu=on" || result=1
+grub2-mkconfig -o /boot/grub2/grub.cfg || result=1
 
-echo "#### Checking for node exporter ####"
-if [ -f /usr/local/bin/node_exporter ]; then
-    echo "Node exporter already installed. Skipping"
-else
-    echo "Installing node exporter"
-
-    cd /opt && mkdir -p /var/lib/node_exporter/textfile_collector && curl -sSfL https://raw.githubusercontent.com/carlocorradini/node_exporter_installer/main/install.sh | INSTALL_NODE_EXPORTER_SKIP_FIREWALL=true INSTALL_NODE_EXPORTER_EXEC="--collector.logind --collector.interrupts --collector.systemd --collector.processes --collector.textfile.directory=/var/lib/node_exporter/textfile_collector" sh -s - 
-    if [ $? -ne 0 ]; then
-        echo "Failed to install node exporter"
-        result=1
-    fi
-    # Go back to script_dir
-    cd ${script_dir}
-fi
 
 echo "#### Identifying system ####"
 
@@ -283,8 +133,8 @@ case "$host" in
         NPFSYSTEM="VMv4r-kvhv"
         ;;
         *)
-        echo "Change etc/netperfect-release to NPF-06 if we are one an original NetPerfect hardware"
-        NPFSYSTEM="NPF06A"
+        echo "Change etc/netperfect-release if we are one an original NetPerfect hardware"
+        NPFSYSTEM="${HARDWARE_ID}"
         ;;
 esac
 
@@ -308,7 +158,7 @@ echo "#### Cleanup system files ####"
 #/bin/rm -rf /var/log/anaconda
 
 if [ $result -ne 0 ]; then
-    echo "#### WARING Installation FAILED ####"
+    echo "#### WARNING Installation FAILED ####"
 else
     echo "#### Installation done (check logs) ####"
     echo "Don't forget to remove this file if it was on disk"
