75776c08f88089f89f8ba80ecdbc72ea72883187
Orsiris de Jong <ozy@netpower.fr>
2024-10-28 21:24:33 +0100

    hypervisor: Improve error handling

--- optional_tasks/setup_hypervisor.sh
+++ optional_tasks/setup_hypervisor.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-## Hypervisor Installer 2024101001 for RHEL9
+## Hypervisor Installer 2024102801 for RHEL9
 
 # Requirements:
 # RHEL9 installed with NPF VMv4 profile incl. node exporter
@@ -36,6 +36,9 @@ function log {
     local log_line="${1}"
     local level="${2}"
 
+    if [ "${level}" != "" ]; then
+        log_line="${level}: ${log_line}"
+    fi
     echo "${log_line}" >> "${LOG_FILE}"
     echo "${log_line}"
 
@@ -43,6 +46,12 @@ function log {
         SCRIPT_GOOD=false
     fi
 }
+
+function log_quit {
+    log "${1}" "${2}"
+    exit 1
+}
+
 echo "#### Installing prerequisites ####"
 
 dnf install -y epel-release || log "Failed to install epel release" "ERROR"
@@ -56,17 +65,17 @@ dnf install -y virt-manager xorg-x11-xauth || log "Failed to install virt-manage
 echo "#### System tuning ####"
 # Don't log martian packets, obviously we'll get plenty
 # These are RHEL specific with ANSSI BP028 high profile
-sysctl -w net.ipv4.conf.all.log_martians=0
+sysctl -w net.ipv4.conf.all.log_martians=0 || log "Cannot set net.ipv4.conf.all.log_martians=0 live" "ERROR"
 # THis is a link
 #sed -i 's/net.ipv4.conf.all.log_martians = 1/net.ipv4.conf.all.log_martians = 0/g' /etc/sysctl.d/99-sysctl.conf
-sed -i 's/net.ipv4.conf.all.log_martians = 1/net.ipv4.conf.all.log_martians = 0/g' /etc/sysctl.conf
+sed -i 's/net.ipv4.conf.all.log_martians = 1/net.ipv4.conf.all.log_martians = 0/g' /etc/sysctl.conf || log "Cannot update /etc/sysctl.conf" "ERROR"
 
 echo "#### Setting up system certificate ####"
 
 [ ! -d "${TARGET_DIR}" ] && mkdir "${TARGET_DIR}"
 
-openssl req -nodes -new -x509 -days 7300 -newkey rsa:4096 -keyout ${CERT_DIR}/private/${COMMON_NAME// /_}.key -subj "${CRT_SUBJECT}" -out ${CERT_DIR}/certs/${COMMON_NAME// /_}.crt  || result=1
-cat ${CERT_DIR}/private/${COMMON_NAME// /_}.key ${CERT_DIR}/certs/${COMMON_NAME// /_}.crt > ${TARGET_DIR}/${COMMON_NAME// /_}.pem
+openssl req -nodes -new -x509 -days 7300 -newkey rsa:4096 -keyout ${CERT_DIR}/private/${COMMON_NAME// /_}.key -subj "${CRT_SUBJECT}" -out ${CERT_DIR}/certs/${COMMON_NAME// /_}.crt  || log "Failed to generate local cert" "ERROR"
+cat ${CERT_DIR}/private/${COMMON_NAME// /_}.key ${CERT_DIR}/certs/${COMMON_NAME// /_}.crt > ${TARGET_DIR}/${COMMON_NAME// /_}.pem || log "Failed to concat local cert" "ERROR"
 
 echo "#### Setup SNMP ####"
 
@@ -84,6 +93,8 @@ view   systemview    included   .1.3.6.1.4.1.2021.9
 # CPU
 view    systemview    included   .1.3.6.1.4.1.2021.10
 EOF
+[ $? -eq 0 ] || log "Failed to create /tmp/snmpdçpart.conf"
+
 sed -i '/^view    systemview    included   .1.3.6.1.2.1.25.1.1$/ r /tmp/snmpd_part.conf' /etc/snmp/snmpd.conf 2>> "${LOG_FILE}" || log "Configuring SNMP failed" "ERROR"
 
 echo "#### Setting up cockpit & performance logging ####"
@@ -101,20 +112,19 @@ systemctl start cockpit.socket || log "Failed to start cockpit" "ERROR"
 #echo 'Defaults:'${ADMIN_USER}' !requiretty' >> /etc/sudoers
 
 #Let's allow cockpit user root (which is okay since we have pam faillock set)
-sed -i 's/^root/#root/g' /etc/cockpit/disallowed_users 2>> "${LOG_FILE}" || log "Allowing root user for cockpit failed" "ERROR"
+sed -i 's/^root/#root/g' /etc/cockpit/disallowed-users 2>> "${LOG_FILE}" || log "Allowing root user for cockpit failed" "ERROR"
 
 
 echo "#### Setup first ethernet interface as bridged to new bridge kvmbr0 ####"
 # ip -br l == ip print brief list of network interfaces
 iface=$(ip -br l | awk '$1 !~ "lo|vir|wl" { print $1; exit }')
 if [ -z ${iface} ]; then
-    echo "Failed to get first ethernet interface"
-    exit 1
+    log_quit "Failed to get first ethernet interface" "ERROR"
 fi
 
 # Disable spanning tree so we don't interrupt existing STP infrastructure
 nmcli c add type bridge ifname kvmbr0 con-name kvmbr0 autoconnect yes bridge.stp no 2>> "${LOG_FILE}" || log "Creating bridge failed" "ERROR"
-nmcli c modify kvmbr0 ipv4.method dhcp  2>> "${LOG_FILE}" || log "Setting bridge ipv4 DHCP failed" "ERROR"
+nmcli c modify kvmbr0 ipv4.method auto 2>> "${LOG_FILE}" || log "Setting bridge ipv4 DHCP failed" "ERROR"
 nmcli c add type bridge-slave ifname ${iface} master kvmbr0 autoconnect yes 2>> "${LOG_FILE}" || log "Adding bridge slave failed" "ERROR"
 nmcli c up kvmbr0  2>> "${LOG_FILE}" || log "Enabling bridge failed" "ERROR"
 nmcli c del ${iface}  2>> "${LOG_FILE}" || log "Deleting interface ${iface} config failed" "ERROR"
@@ -127,12 +137,17 @@ PARALLEL_SHUTDOWN=2
 SHUTDOWN_TIMEOUT=360
 SYNC_TIME=1
 EOF
-systemctl enable --now libvirtd || result=1
-systemctl enable --now libvirt-guests || result=1
+[ $? -eq 0 ] || log "Failed to create /etc/sysconfig/libvirt-guests" "ERROR"
+
+systemctl enable libvirtd || log "Failed to enable libvirtd" "ERROR"
+systemctl start libvirtd || log "Failed to enable libvirtd" "ERROR"
+systemctl enable libvirt-guests || log "Failed to enable libvirt-guests" "ERROR"
+systemctl start libvirt-guests || log "Failed to start libvirt-guests" "ERROR"
+
 
 echo "#### Setup PCI Passthrough ####"
-grubby --update-kernel=ALL --args="intel_iommu=on" || result=1
-grub2-mkconfig -o /boot/grub2/grub.cfg || result=1
+grubby --update-kernel=ALL --args="intel_iommu=on" || log "Failed to add iommu kernel argument" "ERROR"
+grub2-mkconfig -o /boot/grub2/grub.cfg || log "Failed to generate grub.cfg" "ERROR"
 
 
 echo "#### Identifying system ####"
@@ -158,10 +173,10 @@ case "$host" in
         ;;
 esac
 
-echo "NPF-${NPFSYSTEM}" > /etc/netperfect-release
+echo "NPF-${NPFSYSTEM}" > /etc/netperfect-release || log "Failed to create /etc/netperfect-release" "ERROR"
 
 ## Disable sssd
-systemctl disable sssd
+systemctl disable sssd || log "Cannot disable sssd" "ERROR"
 
 
 echo "#### Cleanup system files ####"
@@ -180,11 +195,12 @@ echo "#### Cleanup system files ####"
 /bin/rm -rf /var/lib/authselect/backups/*
 #/bin/rm -rf /var/log/anaconda
 
-if [ $result -ne 0 ]; then
+if [ $SCRIPT_GOOD == false ]; then
     echo "#### WARNING Installation FAILED ####"
+    exit 1
 else
     echo "#### Installation done (check logs) ####"
     echo "Don't forget to remove this file if it was on disk"
     echo "Then reboot this machine"
+    exit 0
 fi
-exit $result
